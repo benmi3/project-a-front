2f039368a1124d5b71dce162ecf583f82af5e448
Benjamin Jørgensen <69441266+benmi3@users.noreply.github.com>
2024-05-10 12:29:22 +0900

    Create ListTask.vue

---
+++ src/components/ListTask.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api/tauri'
+import SingleTask from './SingleTask.vue'
+defineProps<{ msg: string }>()
+
+const taskList = ref(0)
+async function backendTaskList() {
+  taskList = await invoke('tasklist', { title: singletask1 })
+}
+onMount()
+</script>
+
+<template>
+  <div class="w-9/12 mx-auto">
+    <h1>{{ msg }}</h1>
+
+    <li v-for="(task) in taskList">
+       <SingleTask msg="Hello Tauri + Vue!", task=task />
+    </li>
+  </div>
+</template>
+
