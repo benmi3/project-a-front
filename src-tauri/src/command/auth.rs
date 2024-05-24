7578ee4ec7cc8afb0a5c38498b8b38a7905543b0
Benjamin Jørgensen <me@benmi.me>
2024-05-24 21:33:32 +0900

    trying to get the cloned hc to work

--- src-tauri/src/command/auth.rs
+++ src-tauri/src/command/auth.rs
@@ -1,16 +1,11 @@
-use lib_http_client::Client;
 use crate::command::{Error, Result};
 use crate::http_requests::auth;
+use lib_http_client::Client;
 use tauri::State;
 
-
-
 #[tauri::command(async)]
-pub async fn login<'r>(username: &str, password: &str, hc: State<'r, Client>) -> Result<bool> {
+pub async fn login<'r>(username: &str, password: &str, &hc: State<'r, &Client>) -> Result<bool> {
     // TODO: Add username and password symbol check here.
 
-   Ok(auth::login_request(username, password, hc).await?)
-
-
-
+    Ok(auth::login_request(username, password, hc).await?)
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,26 +5,27 @@ use lib_http_client::Client;
 //use tauri::State;
 //use tauri::api::shell;
 //use tauri::{CustomMenuItem, Manager, Menu, Submenu};
+mod command;
 mod error;
 mod http_requests;
-mod command;
 mod model;
 
-use crate::command::{greet, task, backend_add}; // Temp
-use crate::command::auth::{login}; // User Login
+use crate::command::auth::login;
+use crate::command::{backend_add, greet, task}; // Temp // User Login
 
 fn main() {
     let ctx = tauri::generate_context!();
     let hc = create_http_client();
     tauri::Builder::default()
-        .manage(hc)
+        .manage(&hc.clone())
         .invoke_handler(tauri::generate_handler![greet, task, backend_add, login])
         .run(ctx)
         .expect("error while running tauri application");
 }
 
-async fn create_http_client() -> Client{
+async fn create_http_client() -> Client {
     // Will use unwrap here for this, as without a http client
     // There is no way to get any data
     lib_http_client::new_client("http://localhost:8080").unwrap()
 }
+
