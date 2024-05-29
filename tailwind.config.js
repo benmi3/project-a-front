05acf1c9854290a0647441f0ba399e3b88b45c9c
Benjamin Jørgensen <me@benmi.me>
2024-05-29 23:13:54 +0900

    small changes to dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   //purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
- // darkMode: ['selector','[data-mode="dark"]'], // or 'media' or 'class'
+  // darkMode: ['selector','[data-mode="dark"]'], // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }
